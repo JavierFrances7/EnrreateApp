60641c99c08faac4f0d29ff309002e35aecd9663
JAVI-FRANCES\Javier <javierfrances7@gmail.com>
2022-05-09 20:26:22 +0200

    Correción de version de firebase, instalada la 9.7.0 (npm install firebase@9.7.0 --save --save-exact).

--- EnrreateApp/src/app/providers/firebase-auth-service.ts
+++ EnrreateApp/src/app/providers/firebase-auth-service.ts
@@ -7,14 +7,18 @@ export class FirebaseAuthService {
     constructor(public angularFireAuth: AngularFireAuth) {
     }
 
+
+    //Método para registrar nuevos usuarios en Firebase
     registerUser(email: string, password: string): Promise<any> {
         return this.angularFireAuth.createUserWithEmailAndPassword(email, password);
     }
 
+    //Método para iniciar sesión con email y contraseña previamente registrado en Firebase
     loginUser(email: string, password: string): Promise<any> {
         return this.angularFireAuth.signInWithEmailAndPassword(email, password);
     }
 
+    //Método para cerrar sesión del usuario logueado
     logoutUser(): Promise<any> {
         return new Promise((resolve, reject) => {
             if (this.angularFireAuth.currentUser) {  //antes del logout se comprueba que hay usuario logueado
@@ -29,6 +33,7 @@ export class FirebaseAuthService {
         })
     }
 
+    //Método que retorna los detalles del usuario
     userDetails() {
         return this.angularFireAuth.user;
     }
